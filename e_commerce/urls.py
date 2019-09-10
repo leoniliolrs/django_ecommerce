97c09db1e7f8e64771fac606b29575f98663f7d6
CTI <cti@debian.ifpicos.com.br>
2019-09-10 15:47:35 -0300

    update views products/list.html

--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -19,7 +19,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
-from products.views import ProductListView
+from products.views import ProductListView, product_list_view
 from .views import home_page, about_page, contact_page, login_page, register_page
 
 urlpatterns = [
@@ -29,6 +29,7 @@ urlpatterns = [
     path('login/', login_page),
     path('register/', register_page),
     path('products/', ProductListView.as_view()),
+    path('products-fbv/', product_list_view), 
 	path('admin/', admin.site.urls),
 ]
 if settings.DEBUG:
