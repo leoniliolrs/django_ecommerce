c7654160da55df9c4355579c4a56329caca1ca60
admin <leoniliolrs@gmail.com>
2019-09-11 17:44:24 -0300

    update views

--- products/views.py
+++ products/views.py
@@ -34,18 +34,27 @@ def product_list_view(request):
 #Class Based View
 class ProductDetailView(DetailView):
     #traz todos os produtos do banco de dados sem filtrar nada
-    queryset = Product.objects.all()
+    #queryset = Product.objects.all()
     template_name = "products/detail.html"
 
     def get_context_data(self, *args, **kwargs):
         context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
         print(context)
         return context
+    def get_object(self, *args, **kwargs):
+        pk = self.kwargs.get('pk')
+        instance = Product.objects.get_by_id(pk)
+        if instance is None:
+            raise Http404("Esse produto não existe!")
+        return instance
 
 #Function Based View
 def product_detail_view(request, pk = None, *args, **kwargs):
-    #instance = Product.objects.get(pk = pk) #get the object id
-    instance = get_object_or_404(Product, pk = pk)
+    instance = Product.objects.get_by_id(pk)
+    print(instance)
+    if instance is None:
+        raise Http404("Esse produto não existe!")
+
     context = {
         'object': instance
     }
