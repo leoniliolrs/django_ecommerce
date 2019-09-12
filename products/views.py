19a918d2388e11626b9c2189b9841eeb990c3a07
CTI <cti@debian.ifpicos.com.br>
2019-09-12 12:01:29 -0300

    insert blank = True in slug

--- products/views.py
+++ products/views.py
@@ -5,6 +5,17 @@ from django.shortcuts import render, get_object_or_404
 
 from .models import Product
 
+class ProductFeaturedListView(ListView):
+    template_name = "products/list.html"
+
+    def get_queryset(self, *args, **kwargs):
+        request = self.request
+        return Product.objects.featured()
+
+class ProductFeaturedDetailView(DetailView):
+    queryset = Product.objects.all().featured()
+    template_name = "products/featured-detail.html"
+
 #Class Based View
 class ProductListView(ListView):
     #traz todos os produtos do banco de dados sem filtrar nada
@@ -25,9 +36,24 @@ def product_list_view(request):
     }
     return render(request, "products/list.html", context)
 
+class ProductDetailSlugView(DetailView):
+    queryset = Product.objects.all()
+    template_name = "products/detail.html"
+
+    def get_object(self, *args, **kwargs):
+        slug = self.kwargs.get('slug')
+        #instance = get_object_or_404(Product, slug = slug, active = True)
+        try:
+            instance = Product.objects.get(slug = slug, active = True)
+        except Product.DoesNotExist:
+            raise Http404("Não encontrado!")
+        except Product.MultipleObjectsReturned:
+            qs = Product.objects.filter(slug = slug, active = True)
+            instance =  qs.first()
+        return instance
+
 #Class Based View
 class ProductDetailView(DetailView):
-    #queryset = Product.objects.all()
     template_name = "products/detail.html"
 
     def get_context_data(self, *args, **kwargs):
@@ -41,6 +67,7 @@ class ProductDetailView(DetailView):
         if instance is None:
             raise Http404("Esse produto não existe!")
         return instance
+
 
 #Function Based View
 def product_detail_view(request, pk = None, *args, **kwargs):
